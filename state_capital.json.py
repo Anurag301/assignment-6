b19bc2763d8134b4d5b705347f2278a6f530888e
Anurag301 <115408889+Anurag301@users.noreply.github.com>
2022-11-26 19:47:50 +0530

    Add files via upload

---
+++ state_capital.json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+{"Tamilnadu": "Chennai", "Kerala": "Thiruvnanthapuram", "Karnataka": "Bangaluru", "Telangana": "Hyderabad", "Goa": "Panaji", "Uttarpradesh": "Lucknow", "Maharastra": "Mumbai"}
+
